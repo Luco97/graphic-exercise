57a83ce991e1bb763e7097d025e8eaa32b99395c
Luco97 <renatoluco@gmail.com>
2022-07-24 01:53:41 -0400

    ADD se crea bubblesort que ordena de mayor a menor

--- bubble-sort.h
+++ bubble-sort.h
@@ -17,3 +17,20 @@ void BubbleSort(std::vector<std::vector<int>> &coordinates) {
         }
     }
 }
+
+void BubbleSortReverse(std::vector<std::vector<int>>& coordinates) {
+    for (int i = 0; i < coordinates.size(); i++)
+    {
+        for (int j = 1; j < coordinates.size(); j++)
+        {
+            if (coordinates[j - 1][0] < coordinates[j][0]) {
+                int x = coordinates[j - 1][0];
+                int y = coordinates[j - 1][1];
+                coordinates[j - 1][0] = coordinates[j][0];
+                coordinates[j - 1][1] = coordinates[j][1];
+                coordinates[j][0] = x;
+                coordinates[j][1] = y;
+            }
+        }
+    }
+}
