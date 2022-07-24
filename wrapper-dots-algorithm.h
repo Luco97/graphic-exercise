50e7d355d622440624d89ddd538febffc7ff4470
Luco97 <renatoluco@gmail.com>
2022-07-24 01:55:11 -0400

    UPDATE se agrega logica importante para desarrollar problema de forma correcta

--- wrapper-dots-algorithm.h
+++ wrapper-dots-algorithm.h
@@ -5,17 +5,18 @@
 std::vector<std::vector<int>> WrapperDotsAlgorithm(std::vector<std::vector<int>> dots) {
     std::vector<std::vector<int>> borderPoints;
     std::vector<int> basePoint = dots[0];
+    borderPoints.push_back(basePoint);
     std::vector<int> bestPoint;
     std::vector<int> actualPoint;
 
     for (int i = 0; i < dots.size(); i++)
     {
-        bestPoint = dots[i];
+        bestPoint = dots[dots.size() - 1];
 
-        for (int j = 0; j < dots.size(); j++)
+        for (int j = 1; j < dots.size() - 1; j++)
         {
             actualPoint = dots[j];
-            if (crossPoint2D(basePoint[0], basePoint[1], actualPoint[0], actualPoint[1], basePoint[0], basePoint[1], bestPoint[0], bestPoint[1]) > 0) {
+            if (crossPoint2D(basePoint[0], basePoint[1], actualPoint[0], actualPoint[1], basePoint[0], basePoint[1], bestPoint[0], bestPoint[1]) > 0 && i != j) {
                 bestPoint = actualPoint;
             }
         }
