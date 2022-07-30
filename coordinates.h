7c2df4d0fc21202efededaa8f2364256b8cd5bfd
Luco97 <renatoluco@gmail.com>
2022-07-30 17:41:02 -0400

    ADD clase de coordenadas para mini-tarea

---
+++ coordinates.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+
+class coordinates
+{
+private:
+	double x;
+	double y;
+public:
+	coordinates(double x, double y);
+	void setValues(double x, double y);
+	std::vector<double> getValues();
+	bool operator > (coordinates& coor);
+};
+
