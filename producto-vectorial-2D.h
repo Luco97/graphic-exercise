69dc89b4a55cc75750a5106277b359a53f45faa9
Luco97 <renatoluco@gmail.com>
2022-08-03 20:24:12 -0400

    ADD se cambia archivo y nombre de funcion

--- producto-vectorial-2D.h
+++ producto-vectorial-2D.h
@@ -1,6 +1,6 @@
 #pragma once
 
-double crossPoint2D(int xi1, int yi1, int xf1, int yf1, int xi2, int yi2, int xf2, int yf2) {
+double productoVectorial2D(int xi1, int yi1, int xf1, int yf1, int xi2, int yi2, int xf2, int yf2) {
     double x1 = xf1 - xi1;
     double y1 = yf1 - yi1;
     double x2 = xf2 - xi2;
